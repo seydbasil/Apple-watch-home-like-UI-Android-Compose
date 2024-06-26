d4d33adf9a0f115ef2218ecff55cc26ad54cbf66
seydbasil <seyd.mb@accubits.com>
2024-06-26 13:45:31 +0530

    add fillMaxSize which should be added

--- app/src/main/java/com/smbvt/bst/applewatchhomeuiandroid/ui/composables/apple_watch_ui/MenuItem.kt
+++ app/src/main/java/com/smbvt/bst/applewatchhomeuiandroid/ui/composables/apple_watch_ui/MenuItem.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -28,6 +29,7 @@ fun MenuItem(
     val shape = CircleShape
     Image(
         modifier = modifier
+            .fillMaxSize() // fillMaxSize is must here for proper working of items. especially if we need to identify center item
             .padding(Padding5)
             .clip(shape).clickable {
                 onClick()
